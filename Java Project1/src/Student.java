770228bab029fab828bc71bd3f92432be5bb296e
MiaArro0 <arroyo_mia@students.columbusstate.edu>
2026-08-28 19:30:15 -0400

    The Final Grade Calc class has been established, but not the student class.

--- Java Project1/src/Student.java
+++ Java Project1/src/Student.java
@@ -1,2 +1,18 @@
 public class Student {
+    static String name;
+    static int grade;
+
+    public Student(String name, int age){
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public static String getName(){
+        return name;
+    }
+
+    public static int getGrade(){
+        return grade;
+    }
+
 }
